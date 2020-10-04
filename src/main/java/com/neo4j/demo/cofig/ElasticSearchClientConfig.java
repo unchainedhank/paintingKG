fda0bc3671cb277f0396a2a9fabdb10331dfe657
unchainedhank <2450312896@qq.com>
2020-10-04 21:09:12 +0800

    ElasticSearch configuration class line comment completed.

--- src/main/java/com/neo4j/demo/cofig/ElasticSearchClientConfig.java
+++ src/main/java/com/neo4j/demo/cofig/ElasticSearchClientConfig.java
@@ -8,11 +8,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticSearchClientConfig {
-
+    /**
+     * 配置RestHighLevelClient对象，交给spring容器管理
+     * @return RestHighLevelClient
+     */
     @Bean
     public RestHighLevelClient restHighLevelClient() {
         return new RestHighLevelClient(
                 RestClient.builder(
+                        //可以传递多个（数组)
                         new HttpHost("127.0.0.1", 9200, "http")
                 )
         );
