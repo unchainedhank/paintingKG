b3a4ed426823adca22e74b38c2d9bc6eee9a80ce
unchainedhank <2450312896@qq.com>
2020-10-04 21:19:06 +0800

    ElasticSearch test class completed.

--- src/test/java/com/neo4j/demo/ElasticSearchApiTest.java
+++ src/test/java/com/neo4j/demo/ElasticSearchApiTest.java
@@ -1,9 +1,15 @@
 package com.neo4j.demo;
 
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 public class ElasticSearchApiTest {
@@ -11,4 +17,18 @@ public class ElasticSearchApiTest {
     @Autowired
     @Qualifier("restHighLevelClient")
     RestHighLevelClient client;
+
+    /**
+     * 创建索引
+     */
+    @Test
+    void createIndex() throws IOException {
+        //创建索引
+        CreateIndexRequest request = new CreateIndexRequest("ft_idx");
+        //客户端执行请求，获得响应
+        CreateIndexResponse response = client.indices().create(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.index());
+
+    }
 }
