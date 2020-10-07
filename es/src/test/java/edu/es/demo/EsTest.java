48d6ba71b48a0b04ba6a335efff5e16dfa07293c
unchainedhank <2450312896@qq.com>
2020-10-07 10:10:51 +0800

    es addDocument method built.

--- es/src/test/java/edu/es/demo/EsTest.java
+++ es/src/test/java/edu/es/demo/EsTest.java
@@ -1,12 +1,19 @@
 package edu.es.demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.es.demo.POJO.Painter;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -39,6 +46,23 @@ public class EsTest {
         System.out.println(response.isAcknowledged());
     }
 
+    @Test
+    void addDocument() throws IOException {
+        Painter painter = new Painter((long) 123, "马远");
+        IndexRequest request = new IndexRequest("painter_idx");
+        //设置请求
+        request.id("1");
+        request.timeout(TimeValue.timeValueSeconds(1));
+        request.timeout("1s");
+        ObjectMapper mapper = new ObjectMapper();
+        //把json放入请求
+        request.source(mapper.writeValueAsString(painter), XContentType.JSON);
+        //发送请求
+        IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
+
+        System.out.println(indexResponse.toString());
+        System.out.println(indexResponse.status());
+    }
 
 
 }
