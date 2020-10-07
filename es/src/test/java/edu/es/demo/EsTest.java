1d35be03fdaa0931ce4dd5ef7f2d694526b9284c
unchainedhank <2450312896@qq.com>
2020-10-07 10:28:13 +0800

    es single document CRUD method built.

--- es/src/test/java/edu/es/demo/EsTest.java
+++ es/src/test/java/edu/es/demo/EsTest.java
@@ -4,9 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.es.demo.POJO.Painter;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
@@ -14,6 +20,7 @@ import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -62,6 +69,43 @@ public class EsTest {
 
         System.out.println(indexResponse.toString());
         System.out.println(indexResponse.status());
+    }
+
+    @Test
+    void isExists() throws IOException {
+        GetRequest request = new GetRequest("painter_idx", "1");
+        //不获取返回的上下文
+        request.fetchSourceContext(new FetchSourceContext(false));
+
+        boolean exists = client.exists(request, RequestOptions.DEFAULT);
+        System.out.println(exists);
+    }
+
+    @Test
+    void getDocument() throws IOException {
+        GetRequest request = new GetRequest("painter_idx", "1");
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        System.out.println(response.getSourceAsString());
+        System.out.println(response);
+    }
+
+    @Test
+    void updateDocument() throws IOException {
+        UpdateRequest request = new UpdateRequest("painter_idx", "1");
+        Painter painter = new Painter((long) 1234, "卡拉瓦乔");
+        request.timeout("1s");
+        request.doc(new ObjectMapper().writeValueAsString(painter),XContentType.JSON);
+
+        UpdateResponse response = client.update(request,RequestOptions.DEFAULT);
+        System.out.println(response.status());
+    }
+
+    @Test
+    void deleteDocument() throws IOException {
+        DeleteRequest request = new DeleteRequest("painter_idx", "1");
+        request.timeout("1s");
+        DeleteResponse response = client.delete(request,RequestOptions.DEFAULT);
+        System.out.println(response.status());
     }
 
 
