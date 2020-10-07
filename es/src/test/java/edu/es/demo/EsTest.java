2f9102d083b9e471adac9bc7edcc39d0c5bb4768
unchainedhank <2450312896@qq.com>
2020-10-07 18:00:34 +0800

    es single document search method built.

--- es/src/test/java/edu/es/demo/EsTest.java
+++ es/src/test/java/edu/es/demo/EsTest.java
@@ -1,15 +1,17 @@
 package edu.es.demo;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.es.demo.POJO.Painter;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
@@ -20,17 +22,28 @@ import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @SpringBootTest
 public class EsTest {
     @Resource
     private RestHighLevelClient client;
+
+    /**
+     * Painter:
+     *   painter_index: "painter_idx"
+     */
+    final static String painter_index = "painter_idx";
 
     @Test
     void creatIndex() throws IOException {
@@ -48,7 +61,7 @@ public class EsTest {
 
     @Test
     void deleteIndex() throws IOException {
-        DeleteIndexRequest request = new DeleteIndexRequest("ft_idx");
+        DeleteIndexRequest request = new DeleteIndexRequest(painter_index);
         AcknowledgedResponse response = client.indices().delete(request,RequestOptions.DEFAULT);
         System.out.println(response.isAcknowledged());
     }
@@ -56,7 +69,7 @@ public class EsTest {
     @Test
     void addDocument() throws IOException {
         Painter painter = new Painter((long) 123, "马远");
-        IndexRequest request = new IndexRequest("painter_idx");
+        IndexRequest request = new IndexRequest(painter_index);
         //设置请求
         request.id("1");
         request.timeout(TimeValue.timeValueSeconds(1));
@@ -73,7 +86,7 @@ public class EsTest {
 
     @Test
     void isExists() throws IOException {
-        GetRequest request = new GetRequest("painter_idx", "1");
+        GetRequest request = new GetRequest(painter_index, "1");
         //不获取返回的上下文
         request.fetchSourceContext(new FetchSourceContext(false));
 
@@ -108,5 +121,35 @@ public class EsTest {
         System.out.println(response.status());
     }
 
+    //批量导入
+    @Test
+    void BulkRequest(ArrayList<Painter> painters) throws IOException {
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.timeout("10s");
+//        ArrayList<Painter> painters = new ArrayList<>();
+        for (int i = 0; i < painters.size(); i++) {
+            bulkRequest.add(
+                    new IndexRequest("painter_idx")
+//                    .id("" + (i+1))不加就是随机
+                    .source(new ObjectMapper().writeValueAsString(painters.get(i)), XContentType.JSON)
+            );
+        }
+
+        BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+        System.out.println(bulkResponse.hasFailures());
+    }
+
+
+    //搜索
+    @Test
+    void search() {
+        SearchRequest searchRequest = new SearchRequest(painter_index);
+        SearchSourceBuilder searchBuilder = new SearchSourceBuilder();
+        //QueryBuilders.termQuery() 精确查找
+        //QueryBuilders.matchAllQuery() 匹配所有
+        TermQueryBuilder queryBuilder = QueryBuilders.termQuery("name", "卡拉瓦乔");
+        searchBuilder.query(queryBuilder);
+
+    }
 
 }
